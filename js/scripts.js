aab52cdbd57cfce841c216a49cce22d17977287c
Jo Renlee Luna <jo@bluthrive.com>
2024-04-27 14:44:37 +0800

    jquery lesson

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         alert("Hello Apsie Welcome Jquery"); 
+//     });
+
+//     $("#mouseChange").hover(function(){
+//         $(this).css("background-color", "yellow");
+//         }, function(){
+//         $(this).css("background-color", "pink");
+//         });
+//   });
